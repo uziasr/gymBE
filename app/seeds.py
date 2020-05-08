48bad41cf6873a7648646f3210ee50e81161c1a0
Uzias Rivera <uziasrivera@gmail.com>
2020-05-07 20:39:41 -0700

    adding data to seeding file

--- app/seeds.py
+++ app/seeds.py
@@ -4,3 +4,20 @@ major_muscle_group = ['Quadriceps', 'Hamstrings',
                       'Triceps', 'Biceps',
                       'Forearms', 'Trapezius',
                       'Abs']
+
+leg_exercises = [('leg press', 0),('squat', 0), ('leg extensions', 0),
+             ('lunge', 0), ('goblet squat', 0), ('front squat', 0),
+             ('deadlift', 1), ('leg curls', 1), ('Glute-Ham Raise', 1),
+             ('Romanian deadlift', 1), ('hip thrust', 1), ('kettlebell swing', 1),
+             ('standing calf raise', 3), ('seated calf raise', 3),
+             ]
+
+push_exercises = [('barbell bench press', 4), ('dumbbell bench press', 4), ('incline barbell bench press', 4),
+                  ('incline dumbbell bench press', 4), ('decline barbell bench press', 4), ('decline dumbbell bench press', 4),
+                  ('cable crossover', 4), ('dumbbell fly', 4), ('machine flye', 4), ('low cable crossover', 4),
+                  ('underhand bench press', 4), ('wide-grip dips', 4), ('push ups', 4),
+                  ('shoulder barbell press', 6),  ('arnold dumbbell press', 6), ('push press', 6), ('lateral raise', 6),
+                  ('seated dumbbell shoulder press', 6), ('seat barbell shoulder press', 6), ('reverse pec fly', 6),
+                  ('dumbbell lateral raise', 6), ('reverse cable crossover', 6), ('one-arm cable lateral raise', 6),
+                  
+                  ]
