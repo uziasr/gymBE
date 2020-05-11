c8c861944a3cfbd13002c5c1e6f4ec5b1148565c
Uzias Rivera <uziasrivera@gmail.com>
2020-05-11 13:02:02 -0700

    created route to get exercises for a workout by name

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,18 @@
 from app import app, db
-from app.models import User, Muscle, Exercise, Workout, Sets, Workout_Muscle, Workout_Exercise
+from app.models import User, Muscle, Exercise, Workout, Sets, WorkoutMuscle, WorkoutExercise
 from flask import request
 from datetime import datetime
 
-sample_data = ['bill', 'is', 'the', 'best']
-major_muscle_group = ['Quadriceps', 'Hamstrings', 'Calves', 'Chest', 'Back', 'Shoulders', 'Triceps', 'Biceps',
-                      'Forearms', 'Trapezius', 'Abs']
+def jsonify_object(instance, cls):
+    return {i.key: instance.__getattribute__(i.key) for i in cls.__table__.columns}
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return {"message" : ['hello','there']}
 
 
 # prime example of taking in parameters from url
-@app.route('/<name>')
-def hello_name(name):
-    return 'Hello {}'.format(name)
-
-
-@app.route('/hidden')
-def message():
-    return " ".join(sample_data)
 
 @app.route('/muscles')
 def get_muscles():
@@ -46,7 +39,7 @@ def start_workout(id):
         for muscle in muscles_getting_trained:
             current_muscle = Muscle.query.filter_by(name=muscle).first()
             print(current_muscle, current_muscle.id, new_workout.id)
-            new_wm = Workout_Muscle(workout_id=new_workout.id, muscle_group_id=current_muscle.id)
+            new_wm = WorkoutMuscle(workout_id=new_workout.id, muscle_group_id=current_muscle.id)
             db.session.add(new_wm)
         db.session.commit()
     return {"message":"workout created"}
@@ -66,13 +59,13 @@ def end_workout(id):
         "start_time" : current_workout.start_time,
         "end_time" : current_workout.end_time}
 
-@app.route('/workout/<id>/set', methods=['POST'])
+@app.route('/workout/<id>/exercise', methods=['POST'])
 def add_exercise(id):
     # exercise is coming in as a string and should be looked up in table
     # id should either be passed through url or through json
     new_exercise = request.get_json()
     exercise_id = Exercise.query.filter_by(name=new_exercise["exercise"]).first().id
-    new_workout_exercise = Workout_Exercise(workout_id=id, exercise_id=exercise_id, order=new_exercise["order"])
+    new_workout_exercise = WorkoutExercise(workout_id=id, exercise_id=exercise_id, order=new_exercise["order"])
     db.session.add(new_workout_exercise)
     db.session.commit()
     return {
@@ -81,10 +74,21 @@ def add_exercise(id):
         "order": new_exercise["order"]
     }
 
-@app.route('/set', methods=['POST'])
-def add_set():
+@app.route('/workout/<id>/set', methods=['POST'])
+def add_set(id):
     req = request.get_json()
-    new_set = Sets(repetition=req["repetition"], set_order=req["set_order"], weight=req["weight"], unit=req["weight"])
+    new_set = Sets(repetition=req["repetition"], set_order=req["set_order"], weight=req["weight"], unit=req["unit"], workout_exercise_id=id)
     db.session.add(new_set)
-    db.session.add(new_set)
-    return new_set
+    db.session.commit()
+    return jsonify_object(instance=new_set, cls=Sets)
+
+@app.route('/workout/<id>')
+def get_workout(id):
+    # exercise_list = WorkoutExercise.query.get(id)
+    my_workout = WorkoutExercise.query.filter_by(workout_id=id).all()
+    my_workout.sort(key=lambda exercise: exercise.order)
+    exercise_list = [Exercise.query.get(i.exercise_id).name for i in my_workout]
+    return {
+        "exercises": exercise_list
+    }
+
